df16684121acaee7e5eda7dcdec503975bc14aa8
Stefan Rimaila <stefan@rimaila.fi>
2019-02-21 01:09:05 +0200

    test(app): Properly test App

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,8 +1,9 @@
 import * as React from 'karet';
+import { mount } from 'enzyme';
 import App from '../app';
 
 describe('App', () => {
   test('mounts without crashing', () => {
-
+    mount(<App />);
   });
 });
